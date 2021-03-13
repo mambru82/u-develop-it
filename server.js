98b6af1126f20ddbdd7df66aad7abd59371d76fa
mambru <ecastro82@gmail.com>
2021-03-12 20:25:30 -0600

    get candidates, delete candidates

--- server.js
+++ server.js
@@ -21,26 +21,58 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     });
 // });
 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// })
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+     })
+})
+
 //Default response for any other requests(Not Found) Catch all
 
 //GET a single candidate
-db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+    WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
 })
 
+
 //delete a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-    if(err) {
-        console.log(err);
-    }
-    console.log(result, this, this.changes);
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if(err) {
+            res.status(400).json({ error: res.message });
+            return;        
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        })
+    })
 })
+
 app.use((req, res) => {
     res.status(404).end();
 })
